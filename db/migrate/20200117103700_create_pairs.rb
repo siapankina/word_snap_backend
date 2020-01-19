e4f16d7293188f119477574f8298cc7c7d32b664
Qing <chingwang.qw@gmail.com>
2020-01-19 22:23:57 +0000

    add unique constraint on pair migration

--- db/migrate/20200117103700_create_pairs.rb
+++ db/migrate/20200117103700_create_pairs.rb
@@ -1,10 +1,11 @@
 class CreatePairs < ActiveRecord::Migration[6.0]
   def change
     create_table :pairs do |t|
-      t.string :chinese
-      t.string :english
+      t.string :chinese, index: {unique: true}
+      t.string :english, index: {unique: true}
 
       t.timestamps
+      
     end
   end
 end
