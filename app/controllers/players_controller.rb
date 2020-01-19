fd85fa3c0a5e813633cf82001fefeb5c229051f8
Anastasia Pankina <siapankina@gmail.com>
2020-01-19 13:45:57 +0000

    filled PlayersController

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,21 @@
 class PlayersController < ApplicationController
+  def new
+    @player = Player.new
+  end
+
+  def create
+    if Player.exists?(username: params[:username])
+      player = Player.where(username: params[:username]).first
+      render json: player, except: [:created_at, :updated_at], include: :games
+    else
+      player = Player.create(player_params)
+      render json: player, except: [:created_at, :updated_at], include: :games
+    end
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:username)
+  end
 end
